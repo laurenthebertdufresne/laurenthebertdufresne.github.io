a7988a08234ce7f211a6a3a15e069ef58337546b
Louis Roy <louis.roy@cossette.com>
2016-10-02 13:18:21 -0400

    background stuff

--- _pages/contact.md
+++ _pages/contact.md
@@ -5,4 +5,6 @@ permalink: /contact/
 weight: 4
 ---
 
-I currently work at the [Santa Fe Institute.](http://www.santafe.edu/) You can get in touch with me by email: laurent at santafe.edu Past affiliation: Ph.D. in physics from [Prof. Louis J. Dubé's research group](http://dynamica.phy.ulaval.ca/) in the [Physics Department](http://www.phy.ulaval.ca/) at [Université Laval](http://www.ulaval.ca/).
+I currently work at the [Santa Fe Institute.](http://www.santafe.edu/) You can get in touch with me by email: laurent at santafe.edu 
+
+Past affiliation: Ph.D. in physics from [Prof. Louis J. Dubé's research group](http://dynamica.phy.ulaval.ca/) in the [Physics Department](http://www.phy.ulaval.ca/) at [Université Laval](http://www.ulaval.ca/).
--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 // Initiate Turbo
 Turbolinks.start();
 
+var didScroll = false;
+
+function onScroll() {
+    didScroll = true;
+}
+
+function onRender() {
+    if (didScroll) {
+        var progress = window.scrollY / (document.body.scrollHeight) * 100;
+
+        document.body.style.backgroundPosition = 'center ' + (-5 - progress * 2) +  'vw'
+
+        didScroll = false;
+    }
+
+    window.requestAnimationFrame(onRender);
+}
+
 document.addEventListener('turbolinks:load', function() {
     console.log('turbolinks:load');
     $('.site-header').scrollToFixed({
         spacerClass:'site-spacer'
     });
 });
+
+
+window.addEventListener('scroll', onScroll);
+window.addEventListener('resize', onScroll);
+window.requestAnimationFrame(onRender);
